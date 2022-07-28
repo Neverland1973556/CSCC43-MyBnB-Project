4957b7f34ff50079ff1039a51ce410b0f0e696ac
Zhitao Xu <xuzhitao200020612@gmail.com>
2022-07-28 16:35:53 -0400

    change set up sql name

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CSCC43-MyBnB-Project
+
+## Team Member
++ Zhitao Xu
++ Zhentao Fan
+
+## Project Description
++ 
--- project/src/SETUP.sql
+++ project/src/SETUP.sql
@@ -1,10 +1,17 @@
+-- Run this in the mysql terminal beforehand
+
+-- Drop table
 Drop TABLE IF EXISTS Sailors;
+
+-- Create new tables and their schema
 create table Sailors (
     sid int(50) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     sname varchar(100) NOT NULL,
     rating INT NOT NULL,
     age INT NOT NULL
 );
+
+-- Add data
 
 /* POPULATE TABLES BELONGING IN OUR DESIRED SCHEMA */
 
